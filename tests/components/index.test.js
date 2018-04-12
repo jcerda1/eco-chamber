6089503341bab81b8e548430b28371b3f4247142
jcerda <jccer103@gmail.com>
2018-04-12 18:39:39 -0500

    it will have test added

--- tests/components/index.test.js
+++ tests/components/index.test.js
@@ -1,4 +1,11 @@
 import React from 'react';
-//import App from '../../client/src/index.jsx'
+import App from '../../client/src/index.jsx';
+import { shallow } from 'enzyme';
 
- 
+
+
+describe('App should render', () => {
+  it('', () => {
+    const wrapper = shallow(<App/>)
+  })
+})
