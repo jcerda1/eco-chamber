facc70b397eedc562c190a7ef16ec7ccc8c00808
jcerda <jccer103@gmail.com>
2018-04-14 09:44:19 -0500

    it will run a test with jets

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
-import {App} from '../../client/src/index.jsx';
+import { App } from '../client/src/index.jsx';
 
 
 describe('<App />', () => {
-  //it('renders 1 component <App /> ', () => {
-    // const wrapper = shallow(<App />);
-    // expect(wrapper.find('div')).to.have.lengthOf(1)
-     it('should be able to run tests', () => {
-        expect(1 + 2).toEqual(3);
-    });
-
+  it('renders 1 component <App /> ', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('div')).to.have.lengthOf(1)
   });
+
+});
