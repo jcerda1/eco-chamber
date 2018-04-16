937384cb410e88d82b29da810ab27f043764153b
michellelockett <shelldog42@gmail.com>
2018-04-16 11:32:04 -0700

    Configures most simple seed data with proper associations

--- db/index.js
+++ db/index.js
@@ -31,6 +31,7 @@ const Article = sequelize.define('Article', {
   url: Sequelize.STRING,
   title: Sequelize.STRING,
   body: Sequelize.STRING,
+  description: Sequelize.STRING,
   date: Sequelize.STRING,
   sentiment: Sequelize.STRING,
   image: Sequelize.STRING,
@@ -79,16 +80,44 @@ Article.belongsToMany(Category, { through: 'ArticleCategory' });
 ///// USE THIS TO SEED DB ///////
 
 sequelize.sync({ force: true }).then(async () => {
-  await Event.bulkCreate(seed.sampleEvents);
-  await Source.bulkCreate(seed.sampleSources);
-  //const articles = await Article.bulkCreate(seed.sampleArticles);
-  // const categories = await Category.bulkCreate(seed.sampleCategories);
-  // await articles[0].addCategories([categories[0]])
-  // await courses[0].addTags([tags[3]]);
-  // await courses[1].addTags([tags[0]]);
-  // await courses[2].addTags([tags[1]]);
-  // await Step.bulkCreate(seed.sampleSteps);
-  // await Comment.bulkCreate(seed.sampleComments);
+  const events = await Event.bulkCreate(seed.sampleEvents);
+  const sources = await Source.bulkCreate(seed.sampleSources);
+  const articles = await Article.bulkCreate(seed.sampleArticles);
+
+  //associate articles with news outlets
+
+  await sources[0].addArticle(articles[0]);
+  await sources[0].addArticle(articles[7]);
+  await sources[1].addArticle(articles[1]);
+  await sources[1].addArticle(articles[8]);
+  await sources[2].addArticle(articles[2]);
+  await sources[2].addArticle(articles[9]);
+  await sources[3].addArticle(articles[3]);
+  await sources[3].addArticle(articles[10]);
+  await sources[4].addArticle(articles[4]);
+  await sources[4].addArticle(articles[11]);
+  await sources[5].addArticle(articles[5]);
+  await sources[5].addArticle(articles[12]);
+  await sources[6].addArticle(articles[6]);
+  await sources[6].addArticle(articles[13]);
+
+  //associate articles with first sample event
+  await events[0].addArticle(articles[0]);
+  await events[0].addArticle(articles[1]);
+  await events[0].addArticle(articles[2]);
+  await events[0].addArticle(articles[3]);
+  await events[0].addArticle(articles[4]);
+  await events[0].addArticle(articles[5]);
+  await events[0].addArticle(articles[6]);
+
+  //associate articles with second sample event
+  await events[1].addArticle(articles[7]);
+  await events[1].addArticle(articles[8]);
+  await events[1].addArticle(articles[9]);
+  await events[1].addArticle(articles[10]);
+  await events[1].addArticle(articles[11]);
+  await events[1].addArticle(articles[12]);
+  await events[1].addArticle(articles[13]); 
 });
 
 ///////////////////////////////
--- db/testData.js
+++ db/testData.js
@@ -55,153 +55,167 @@ const sampleSources = [
   } 
 ];
 
-module.exports = {
-  sampleEvents,
-  sampleSources
-}
+
 
 const sampleArticles = [
   {
-    id: 1,
-    sourceId: 1,
-    eventId: 1,
-    published: "2018-04-11",
+    // sourceId: 1,
+    // eventId: 1,
+    uri: "sampleUri1",
+    date: "2018-04-11",
     title:"Speaker Paul Ryan Retires: ‘This Year Will Be My Last as a Member of the House’",
     description:"House Speaker Paul Ryan revealed at a press conference that he will retire at the end of the congressional term, saying that this “year will be my last as a member of the House.”",
     body: "BODY WILL GO HERE",
     url: "http://www.breitbart.com/big-government/2018/04/11/speaker-paul-ryan-retires-year-will-last-member-house"
   },
   {
-    id: 2,
-    sourceId: 2,
-    eventId: 1,
-    published: "2018-04-11",
+    // sourceId: 2,
+    // eventId: 1,
+    uri: "sampleUri2",
+    date: "2018-04-11",
     title:"House Speaker Paul Ryan will not run for re-election",
     description:"House Speaker Paul Ryan, R-Wis., announced Wednesday that he is not seeking re-election in November -- a move that ends a nearly two decade career in Congress and comes as the GOP girds for a tough fight to keep control of the House this year.",
     body: "BODY WILL GO HERE",
     url: "http://www.foxnews.com/politics/2018/04/11/house-speaker-paul-ryan-wont-run-for-re-election-ap-reports.html"
   },
   {
-    id: 3,
-    sourceId: 3,
-    eventId: 1,
-    published: "2018-04-11",
+    // sourceId: 3,
+    // eventId: 1,
+    uri: "sampleUri3",
+    date: "2018-04-11",
     title:"Ryan to retire as Speaker in January",
     description:"Speaker Paul Ryan (R-Wis.) is retiring at the end of this Congress, ending his speakership a little more than three years after it began and as the GOP faces a possible loss of its majority this fall.",
     body: "BODY WILL GO HERE",
     url: "http://thehill.com/homenews/house/382610-ryan-announces-he-wont-seek-reelection"
   },
   {
-    id: 4,
-    sourceId: 4,
-    eventId: 1,
-    published: "2018-04-11",
+    // id: 4,
+    // sourceId: 4,
+    // eventId: 1,
+    uri: "sampleUri4",
+    date: "2018-04-11",
     title:"Ryan Found Himself on the Margins as G.O.P. Embraces Trump",
     description:"Mr. Ryan said Wednesday that he would not seek re-election, ending a brief stint atop the House and signaling the peril that the Republican majority faces in the midterm elections",
     body: "BODY WILL GO HERE",
     url: "https://www.nytimes.com/2018/04/11/us/politics/paul-ryan-speaker.html"
   },
   {
-    id: 5,
-    sourceId: 5,
-    eventId: 1,
-    published: "2018-04-11",
+    // id: 5,
+    // sourceId: 5,
+    // eventId: 1,
+    uri: "sampleUri5",
+    date: "2018-04-11",
     title:"Paul Ryan announces he won’t run for reelection",
     description: "Paul Ryan announced he would not run for reelection after House Republicans’ weekly gathering on Wednesday, confirming months of speculation that his time in Washington was coming to an end. He will still serve the rest of his term.",
     body: "BODY WILL GO HERE",
     url: "https://www.vox.com/policy-and-politics/2018/4/11/17225202/paul-ryan-out-retirement-announcement"
   },
   {
-    id: 6,
-    sourceId: 6,
-    eventId: 1,
-    published: "2018-04-11",
+    // id: 6,
+    // sourceId: 6,
+    // eventId: 1,
+    uri: "sampleUri6",
+    date: "2018-04-11",
     title:"House Speaker Paul Ryan to exit stage right",
     description: "There’s been an enormous number of House Republican retirement announcements in recent months, but some are more notable than others.",
     body: "BODY WILL GO HERE",
     url: "http://www.msnbc.com/rachel-maddow-show/house-speaker-paul-ryan-exit-stage-right"   
   },
   {
-    id: 7,
-    sourceId: 7,
-    eventId: 1,
-    published: "2018-04-11",
+    // id: 7,
+    // sourceId: 7,
+    // eventId: 1,
+    uri: "sampleUri7",
+    date: "2018-04-11",
     title:"Paul Ryan Says He Won’t Seek Re-Election",
     description: "House Speaker Paul Ryan (R-Wis.) told colleagues Wednesday morning that he will not seek re-election this year and will retire from the House in January, capping off months of rumors that he was mulling stepping down from his leadership post.",
     body: "BODY WILL GO HERE",
     url: "https://www.huffingtonpost.com/entry/paul-ryan-reelection_us_5a328e40e4b07ff75b00d34a"   
   },
   {
-    id: 8,
-    sourceId: 1,
-    eventId: 2,
-    published: "2018-04-11",
+    // id: 8,
+    // sourceId: 1,
+    // eventId: 2,
+    uri: "sampleUri8",
+    date: "2018-04-11",
     title:"Pope Francis admits ‘serious mistakes’ in Chile sex abuse cases",
     description: "Pope Francis told bishops in Chile he feels “pain and shame” for the “crucified lives” of clergy-related sex abuse victims, saying he made “serious mistakes” in his handling of the cases.",
     body: "BODY WILL GO HERE",
     url: "http://www.breitbart.com/news/pope-francis-admits-serious-mistakes-in-chile-sex-abuse-cases"
   },
   {
-    id: 9,
-    sourceId: 2,
-    eventId: 2,
-    published: "2018-04-11",
+    // id: 9,
+    // sourceId: 2,
+    // eventId: 2,
+    uri: "sampleUri9",
+    date: "2018-04-11",
     title:"The Latest: Pope admits 'grave errors' in Chile abuse case",
     description:"Pope Francis has admitted he made \"grave errors\" in judgment in Chile's sex abuse scandal and invited the abuse victims he had discredited to Rome to beg their forgiveness.",
     body: "BODY WILL GO HERE",
     url: "http://www.foxnews.com/world/2018/04/11/latest-pope-admits-grave-errors-in-chile-abuse-case.html"
   },
   {
-    id: 10,
-    sourceId: 3,
-    eventId: 2,
-    published: "2018-04-11",
+    // id: 10,
+    // sourceId: 3,
+    // eventId: 2,
+    uri: "sampleUri10",
+    date: "2018-04-11",
     title:"Did not report",
     description: "Did not report",
     body: "BODY WILL GO HERE",
     url: ""
   },
   {
-    id: 11,
-    sourceId: 4,
-    eventId: 2,
-    published: "2018-04-11",
+    // id: 11,
+    // sourceId: 4,
+    // eventId: 2,
+    uri: "sampleUri11",
+    date: "2018-04-11",
     title:"Pope Francis Admits ‘Grave Errors’ in Chile Sex Abuse Cases",
     description:"Pope Francis has apologized for “grave errors” in the handling of sexual abuse cases in Chile, where he had adamantly defended a bishop accused of covering up abuse by the country’s most notorious pedophile priest.",
     body: "BODY WILL GO HERE",
     url: "https://www.nytimes.com/2018/04/11/world/americas/pope-francis-sex-abuse-apology.html"
   },
   {
-    id: 12,
-    sourceId: 5,
-    eventId: 2,
-    published: "2018-04-11",
+    // id: 12,
+    // sourceId: 5,
+    // eventId: 2,
+    uri: "sampleUri12",
+    date: "2018-04-11",
     title:"Did not report",
     description: "Did not report",
     body: "BODY WILL GO HERE",
     url: ""
   },
   {
-    id: 13,
-    sourceId: 6,
-    eventId: 2,
-    published: "2018-04-11",
+    // id: 13,
+    // sourceId: 6,
+    // eventId: 2,
+    uri: "sampleUri13",
+    date: "2018-04-11",
     title:"Pope acknowledges 'grave errors' in Chilean sex abuse scandal",
     description: "Francis summoned all of Chile's bishops to the Vatican for an emergency meeting, and invited victims to the Holy See to beg their forgiveness.",
     body: "BODY WILL GO HERE",
     url: "https://www.nbcnews.com/news/world/pope-acknowledges-grave-errors-chilean-sex-abuse-scandal-n865141"   
   },
   {
-    id: 14,
-    sourceId: 7,
-    eventId: 2,
-    published: "2018-04-11",
+    // id: 14,
+    // sourceId: 7,
+    // eventId: 2,
+    uri: "sampleUri14",
+    date: "2018-04-11",
     title:"Pope Admits He Made ‘Serious Errors’ In Handling Chile Sex Abuse Allegations",
     description: "In a rare move, the pontiff is summoning all Chilean bishops to Rome to discuss the scandal.",
     body: "BODY WILL GO HERE",
     url: "https://www.huffingtonpost.com/entry/pope-admits-serious-mistakes-chilean-abuse-scandal_us_5ace716ae4b0701783aaf630"   
   }
 ];
+
+module.exports = {
+  sampleEvents,
+  sampleSources,
+  sampleArticles
+}
 
 
 
