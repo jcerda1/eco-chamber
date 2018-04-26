ea27c4b209f5aaea4cae39ddf13367eb560c9983
jcerda <jccer103@gmail.com>
2018-04-26 16:39:27 -0500

    continuing to add style to aritcles component, setting up responsive grid

--- client/src/Articles.jsx
+++ client/src/Articles.jsx
@@ -6,9 +6,6 @@ import React from 'react';
 const Articles = (props) => {
   return (
     <div className="articles-container">
-      <div className="img-container">
-        {props.img}
-      </div>
       <div className="articles">
         {props.articles}
       </div>
--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -19,18 +19,22 @@ let events = props.state.events.map((event, index) => {
   	return <span className="event" key={index}><Link to="/articles">{event.summary}</Link></span>
 })
 let articles = props.state.articles.map((article, index) => {
-  	return <div src={article.img} key={index}>{article.description}</div>
+  	return <div key={index}>
+             <div className="images">
+               <img src={article.img}/>
+             </div>
+             <div className="articles" >
+               {article.description}
+             </div> 
+           </div>
 })
-let img = props.state.articles.map((img, index) => {
-    return <img src={img.img} key={index}/>
-}) 
 
   return (
     <main> 
       <Switch> 
         <Route exact path='/' render={props => <Catagories {...props} cat={categories}/>}></Route> 
         <Route path='/events' render={props => <Events {...props} events={events}/>}></Route> 
-        <Route path='/articles' render={props => <Articles {...props} img={img} articles={articles}/>}></Route> 
+        <Route path='/articles' render={props => <Articles {...props} articles={articles}/>}></Route> 
         <Route path='/signup' render={props => <Signup {...props} />}></Route> 
         <Route path='/signin' render={props => <Signin {...props} />}></Route> 
       </Switch>
