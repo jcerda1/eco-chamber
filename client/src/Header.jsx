6179db4be797c5e72c2e6f7b3abab012cd800e4c
jcerda <jccer103@gmail.com>
2018-04-26 20:19:31 -0500

    removed style import and moved to index.jsx

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { Route } from 'react-router-dom';
-import styles from './styles/index.css'
 import Search from './Search.jsx'
 
 
