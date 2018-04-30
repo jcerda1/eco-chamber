fb8534e3042353856147efd26784ff49465c070c
AlexWellsHS <alex@headspace.com>
2018-04-29 20:38:46 -0700

    add basics /categories, /events, /articles endpoints

--- server/index.js
+++ server/index.js
@@ -11,24 +11,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/dist'));
 
 // categories
-// app.get('/categories', wrap(async (req, res) => {
-//   const categories = await db.Category.findAll();
-//   res.json(categories);
-// }));
+app.get('/categories', wrap(async (req, res) => {
+  const categories = await db.Category.findAll();
+  res.json(categories);
+}));
 
 // events
-// app.get('/events', wrap(async (req, res) => {
-//   const { categoryId } = req.query;
-//   let events = [];
+app.get('/events', wrap(async (req, res) => {
+  const { categoryId } = req.query;
+  const { Subcategories } = await db.Category.findById(categoryId, { include: [{ model: db.Subcategory, include: db.Event }] });
+  let events = [];
+  for (let i = 0; i < Subcategories.length; i++) {
+    events = events.concat(Subcategories[i].Events);
+  }
+  res.json(events);
+}));
 
-//   if (categoryId) {
-//     const category = await db.Category.findById(categoryId);
-//     events = await category.getEvents();
-//   } else {
-//     events = await db.Event.findAll();
-//   }
+// articles
+app.get('/articles', wrap(async (req, res) => {
+  const { eventId } = req.query;
+  const articles = await db.Article.findAll({ where: { eventId } });
+  res.json(articles);
+}));
 
-//   res.json(events);
-// }));
-console.log(process.env);
 app.listen(3000, () => console.log('Listening on port 3000!'));
