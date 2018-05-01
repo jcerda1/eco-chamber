644862b629bac8f835c696e63182989fc9a49b9b
jcerda <jccer103@gmail.com>
2018-05-01 18:32:05 -0500

    added color to background

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -4,35 +4,31 @@ import { Link } from 'react-router-dom'
 
 
 const Signup = () => (
- 
-<form>
-  <div>
-    <h1>Register</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr/>
+  <form>
+    <div>
+      <h1>Register</h1>
+      <p>Please fill in this form to create an account.</p>
+      <hr/>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required></input>
+      <label for="email"><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" required></input>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required></input>
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required></input>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required></input>
-    <hr/>
-    <p>By creating an account you agree to our <Link to="/">Terms & Privacy</Link>.</p>
+      <label for="psw-repeat"><b>Repeat Password</b></label>
+      <input type="password" placeholder="Repeat Password" name="psw-repeat" required></input>
+      <hr/>
+      <p>By creating an account you agree to our <Link to="/">Terms & Privacy</Link>.</p>
 
-    <button type="submit" class="registerbtn">Register</button>
-  </div>
-  
-  <div class="container-signin">
-    <p>Already have an account? <Link to="/signin">Sign in</Link>.</p>
-  </div>
-</form>
-
-
+      <button type="submit" class="registerbtn">Register</button>
+    </div>
+    <div class="container-signin">
+      <p>Already have an account? <Link to="/signin">Sign in</Link>.</p>
+    </div>
+  </form>
 )
 
-
+ 
 
 export default Signup; 
