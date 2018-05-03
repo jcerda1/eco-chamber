c8eac3fb325233412000d3cce6258233c9ceb5a3
jcerda <jccer103@gmail.com>
2018-05-02 19:42:35 -0500

    renders current category atop articles page

--- client/src/Articles.jsx
+++ client/src/Articles.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 const Articles = (props) => {
   return (
   	<div>
-  	<div className="articles-event"> Title </div>
+  	<h1 className="articles-event">{props.currentCat}</h1>
     <ul className="articles-container">
       {props.articles}
     </ul> 
