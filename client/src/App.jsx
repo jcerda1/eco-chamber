9c8a4ab5d51f101cc205ff1479a0b87d3a9b1891
jcerda <jccer103@gmail.com>
2018-05-03 19:15:11 -0500

    cleaned up app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,56 +7,39 @@ import { get } from './helpers/api';
 
 class App extends Component { 
   constructor(props) { 
-
     super(props);
     this.state = { 
       articles: sampleSources.sampleArticles, 
       events: sampleSources.sampleEvents,
-      sources: sampleSources.sampleSources 
-    };
-  }
-
-  componentDidMount() {
-    // get('/categories').then(categories => console.log(categories));
-    // get('/events', { categoryId: 1 }).then(events => console.log(events));
-    // get('/articles', { eventId: 1 }).then(articles => console.log(articles));
-  }
-
-  super(props);
-  this.state = { 
-    articles: sampleSources.sampleArticles, 
-    events: sampleSources.sampleEvents,
-    sources: sampleSources.sampleSources,
-    categories: sampleSources.sampleCategories,
-    currentCat: "Arts"
+      sources: sampleSources.sampleSources,
+      categories: sampleSources.sampleCategories,
+      currentCat: "Arts"
   } 
   this.handleClickCat = this.handleClickCat.bind(this);
   console.log(this.state)
 }
 
+componentDidMount() {
+    // get('/categories').then(categories => console.log(categories));
+    // get('/events', { categoryId: 1 }).then(events => console.log(events));
+    // get('/articles', { eventId: 1 }).then(articles => console.log(articles));
+  }
 
-  handleClickCat(e) {
-    let temp = this.state.currentCat;
-    temp = e.target.id
+handleClickCat(e) {
+  let temp = this.state.currentCat;
+  temp = e.target.id
     this.setState({currentCat: temp});
     console.log(this.state)
-  }
+}
 
   render() {  
     return (
-
       <div className="onboarding">
-        <Header/>
+        <Header state={this.state} catClick={this.handleClickCat}/>
         <Main state={this.state}/>
       </div>
-
-        <div className="onboarding">
-          <Header state={this.state} catClick={this.handleClickCat}/>
-          <Main state={this.state}/>
-        </div>
-
     )  
-  };   
+  }  
 }
 
 export default App;
