b3cb8f44e75063b2efd771da31432cbbd6a48f74
jcerda <jccer103@gmail.com>
2018-05-03 19:31:05 -0500

    removed force true from db file

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('../swagger.json')));
 
 // categories
 <<<<<<< HEAD
+<<<<<<< HEAD
 app.get('/api/categories', wrap(async (req, res) => {
   const categories = await db.Category.findAll();
   res.json(categories);
@@ -71,7 +72,32 @@ app.get('/api/events', wrap(async (req, res) => {
 //   res.json(events);
 // }));
 >>>>>>> 43f58e4... will render categories
+=======
+app.get('/categories', wrap(async (req, res) => {
+  const categories = await db.Category.findAll();
+  res.json(categories);
+}));
 
+// events
+app.get('/events', wrap(async (req, res) => {
+  const { categoryId } = req.query;
+  // TODO: limit events to last X days
+  const { Subcategories } = await db.Category.findById(categoryId, {
+    include: [{
+      model: db.Subcategory,
+      include: db.Event,
+    }],
+  });
+
+  let events = [];
+  for (let i = 0; i < Subcategories.length; i++) {
+    events = events.concat(Subcategories[i].Events);
+  }
+  res.json(events);
+}));
+>>>>>>> 52e83c0... removed force true from db file
+
+<<<<<<< HEAD
 // sources
 app.get('/api/sources', wrap(async (req, res) => {
   const { eventId } = req.query;
@@ -87,6 +113,23 @@ app.get('/api/sources', wrap(async (req, res) => {
     }],
   });
   res.json(sources);
+=======
+// articles
+app.get('/articles', wrap(async (req, res) => {
+  const { eventId } = req.query;
+
+  const sourceUris = ['foxnews.com', 'breitbart.com', 'huffingtonpost.com', 'msnbc.com', 'thehill.com', 'hosted.ap.org', 'nytimes.com'];
+  const sources = await db.Source.findAll({ where: { uri: sourceUris } });
+  const sourceIds = sources.map(source => source.dataValues.id);
+
+  const articles = await db.Article.findAll({
+    where: {
+      eventId,
+      sourceId: sourceIds,
+    }
+  });
+  res.json(articles);
+>>>>>>> d601c5f... removed force true from db file
 }));
 
 // serve index.html
