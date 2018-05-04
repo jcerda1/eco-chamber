abf5a76f650afc4cc4ac346befeda4b3c3539b6c
michellelockett <shelldog42@gmail.com>
2018-05-04 10:43:40 -0700

    Remove lodash dependency, clean up file.  Untested

--- lambda/eventInfo/index.js
+++ lambda/eventInfo/index.js
@@ -1,43 +1,32 @@
 exports.handler = async (event, context) => {
-  const eventsInfo = await getEventInfo(event.uris); 
-  return {events: eventsInfo};
+  const chunked = await getEventsByChunk(uriList, 50);
+  return chunked;
 };
 
 //event registry API
-const { EventRegistry, QueryEventsIter, ReturnInfo, QueryItems, QueryEvents, RequestEventsUriWgtList } = require('eventregistry');
+const { EventRegistry, QueryEvents } = require('eventregistry');
 const er = new EventRegistry({apiKey: process.env.EVENT_REGISTRY_API_KEY});
 
 //lodash
-const _ = require('lodash');
-
-const getEventInfo = async(uriList) => {
-  const q = QueryEvent.initWithEventUriList(uriList);
-  const response = await er.execQuery(q);
-  return response.events.results
-}
-
-//note, above is untested, waiting to have more tokens available
+const { chunk } = require('lodash');
 
 //helper function to retrive detailed event info by event uri list
-// const getEventInfo = async(uriList) => {
-//   let unsavedEvents = [];
-//   const q = new QueryEventsIter.initWithEventUriList(uriList);
-//   return er.execQuery(q).then(events => {
-//     for (const x of events.events.results) {
-//       unsavedEvents.push(x);
-//     }
-//     return unsavedEvents;
-//   }).catch(err => console.log(err)); 
-// };
+const getEventInfo = async(uriList) => {
+  let unsavedEvents = [];
+  const q = QueryEvents.initWithEventUriList(uriList);
+  const response = await er.execQuery(q);
+  return response.events.results;
+};
 
-// const getEventsByChunk = async(uriList) => {
-//   let returnEvents = [];
-//   const chunks = _.chunk(uriList, 20);
+//ER has a max of 50 uris per query
+const getEventsByChunk = async(uriList) => {
+  let returnEvents = [];
+  const chunks = chunk(uriList, 50);
 
-//   for (const chunk of chunks) {
-//     let current = await getEventInfo(chunk); 
-//     returnEvents = returnEvents.concat(current)
-//   }
+  for (const chunk of chunks) {
+    let current = await getEventInfo(chunk); 
+    returnEvents = returnEvents.concat(current)
+  }
 
-//   return returnEvents;
-// }
+  return returnEvents;
+}
