d601c5f5d8c902777afe18482b0c6031933b25c8
jcerda <jccer103@gmail.com>
2018-05-03 19:31:05 -0500

    removed force true from db file

--- db/index.js
+++ db/index.js
@@ -1,91 +1,91 @@
-// const { db_name, db_user, db_password, db_host } = process.env;
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(db_name, db_user, db_password, {
-//   dialect: 'mysql',
-//   host: db_host,
-//   logging: false,
-//   operatorsAliases: false,
-// });
+const { db_name, db_user, db_password, db_host } = process.env;
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(db_name, db_user, db_password, {
+  dialect: 'mysql',
+  host: db_host,
+  logging: false,
+  operatorsAliases: false,
+});
 
-// const seed = require('./testData.js');
+const seed = require('./testData.js');
 
-// const Event = sequelize.define('Event', {
-//   uri: {
-//     type: Sequelize.STRING,
-//     unique: true,
-//     allowNull: false
-//   },
-//   title: {
-//     type: Sequelize.STRING,
-//   },
-//   summary: {
-//     type: Sequelize.TEXT,
-//   },
-//   date: Sequelize.DATE
-// });
+const Event = sequelize.define('Event', {
+  uri: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  title: {
+    type: Sequelize.STRING,
+  },
+  summary: {
+    type: Sequelize.TEXT,
+  },
+  date: Sequelize.DATE
+});
 
-// const Source = sequelize.define('Source', {
-//   uri: Sequelize.STRING,
-//   title: Sequelize.STRING,
-//   importance: Sequelize.INTEGER,
-//   image: Sequelize.STRING,
-//   thumbImage: Sequelize.STRING,
-//   bias: Sequelize.INTEGER
-// });
+const Source = sequelize.define('Source', {
+  uri: Sequelize.STRING,
+  title: Sequelize.STRING,
+  importance: Sequelize.INTEGER,
+  image: Sequelize.STRING,
+  thumbImage: Sequelize.STRING,
+  bias: Sequelize.INTEGER
+});
 
-// const Article = sequelize.define('Article', {
-//   uri: {
-//     type: Sequelize.STRING,
-//     unique: true,
-//     allowNull: false
-//   },
-//   url: Sequelize.STRING,
-//   title: Sequelize.STRING,
-//   body: Sequelize.STRING,
-//   description: Sequelize.STRING,
-//   date: Sequelize.STRING,
-//   sentiment: Sequelize.STRING,
-//   image: Sequelize.STRING,
-// });
+const Article = sequelize.define('Article', {
+  uri: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  url: Sequelize.STRING,
+  title: Sequelize.STRING,
+  body: Sequelize.STRING,
+  description: Sequelize.STRING,
+  date: Sequelize.STRING,
+  sentiment: Sequelize.STRING,
+  image: Sequelize.STRING,
+});
 
-// const Concept = sequelize.define('Concept', {
-//   description: Sequelize.STRING,
-//   uri: {
-//     type: Sequelize.STRING,
-//     unique: true,
-//     allowNull: false
-//   },
-//   type: Sequelize.STRING,
-// });
+const Concept = sequelize.define('Concept', {
+  description: Sequelize.STRING,
+  uri: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  type: Sequelize.STRING,
+});
 
-// const Category = sequelize.define('Category', {
-//   name: Sequelize.STRING,
-// });
+const Category = sequelize.define('Category', {
+  name: Sequelize.STRING,
+});
 
-// const Subcategory = sequelize.define('Subcategory', {
-//   uri: {
-//     type: Sequelize.STRING,
-//     unique: true,
-//     allowNull: false
-//   },
-// });
+const Subcategory = sequelize.define('Subcategory', {
+  uri: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+});
 
-// Event.hasMany(Article);
-// Article.belongsTo(Event);
+Event.hasMany(Article);
+Article.belongsTo(Event);
 
-// Source.hasMany(Article);
-// Article.belongsTo(Source);
+Source.hasMany(Article);
+Article.belongsTo(Source);
 
-// Concept.belongsToMany(Event, {through: 'EventConcept'});
-// Event.belongsToMany(Concept, {through: 'EventConcept'});
+Concept.belongsToMany(Event, {through: 'EventConcept'});
+Event.belongsToMany(Concept, {through: 'EventConcept'});
 
-// Subcategory.belongsToMany(Event, {through: 'EventSubcategory'});
-// Event.belongsToMany(Subcategory, {through: 'EventSubcategory'});
+Subcategory.belongsToMany(Event, {through: 'EventSubcategory'});
+Event.belongsToMany(Subcategory, {through: 'EventSubcategory'});
 
-// Category.hasMany(Subcategory);
-// Subcategory.belongsTo(Category);
+Category.hasMany(Subcategory);
+Subcategory.belongsTo(Category);
 
-// //HELPER FUNCTIONS FOR TESTING
+//HELPER FUNCTIONS FOR TESTING
 
 // const clearDB = () => {
 //   return sequelize
@@ -96,94 +96,94 @@
 //   }).catch(err => console.log("query err: ", err));
 // };
 
-// // const clearDB = () => {
-// //   return sequelize.sync({force:true}).then(async() => {
-// //     await Category.bulkCreate(seed.sampleCategories);
-// //   }).catch(err => console.log("plain DB err: ", err));
-// // }
+// const clearDB = () => {
+//   return sequelize.sync({force:true}).then(async() => {
+//     await Category.bulkCreate(seed.sampleCategories);
+//   }).catch(err => console.log("plain DB err: ", err));
+// }
 
-// const clearTable = (tableName) => {
-//   return  tableName.destroy({
-//     where: {}
-//   });
+const clearTable = (tableName) => {
+  return  tableName.destroy({
+    where: {}
+  });
+};
+
+// const getEventsWithArticles = async (baseCategory) => {
+//   //to do, helper functino to get all events that match the category
+
 // };
 
-// // const getEventsWithArticles = async (baseCategory) => {
-// //   //to do, helper functino to get all events that match the category
+///// USE THIS TO SEED DB ///////
 
-// // };
+// sequelize.sync({ force: true }).then(async () => {
 
-// ///// USE THIS TO SEED DB ///////
+//   const categories = await Category.bulkCreate(seed.sampleCategories);
+//   console.log('synced');
+// });
 
-// // sequelize.sync({ force: true }).then(async () => {
-
-// //   const categories = await Category.bulkCreate(seed.sampleCategories);
-// //   console.log('synced');
-// // });
-
-//  sequelize.sync({ force: true }).then(() => { console.log('db synced')});
+ // sequelize.sync({ force: true }).then(() => { console.log('db synced')});
 
 
-// // sequelize.sync({ force: true }).then(async () => {
-// //   const events = await Event.bulkCreate(seed.sampleEvents);
-// //   const sources = await Source.bulkCreate(seed.sampleSources);
-// //   const articles = await Article.bulkCreate(seed.sampleArticles);
-// //   const categories = await Category.bulkCreate(seed.sampleCategories);
-// //   const subcategories = await Subcategory.bulkCreate(seed.sampleSubcategories);
+// sequelize.sync({ force: true }).then(async () => {
+//   const events = await Event.bulkCreate(seed.sampleEvents);
+//   const sources = await Source.bulkCreate(seed.sampleSources);
+//   const articles = await Article.bulkCreate(seed.sampleArticles);
+//   const categories = await Category.bulkCreate(seed.sampleCategories);
+//   const subcategories = await Subcategory.bulkCreate(seed.sampleSubcategories);
 
-// //   // associate articles with news outlets
+//   // associate articles with news outlets
 
-// //   await sources[0].addArticle(articles[0]);
-// //   await sources[0].addArticle(articles[7]);
-// //   await sources[1].addArticle(articles[1]);
-// //   await sources[1].addArticle(articles[8]);
-// //   await sources[2].addArticle(articles[2]);
-// //   await sources[2].addArticle(articles[9]);
-// //   await sources[3].addArticle(articles[3]);
-// //   await sources[3].addArticle(articles[10]);
-// //   await sources[4].addArticle(articles[4]);
-// //   await sources[4].addArticle(articles[11]);
-// //   await sources[5].addArticle(articles[5]);
-// //   await sources[5].addArticle(articles[12]);
-// //   await sources[6].addArticle(articles[6]);
-// //   await sources[6].addArticle(articles[13]);
+//   await sources[0].addArticle(articles[0]);
+//   await sources[0].addArticle(articles[7]);
+//   await sources[1].addArticle(articles[1]);
+//   await sources[1].addArticle(articles[8]);
+//   await sources[2].addArticle(articles[2]);
+//   await sources[2].addArticle(articles[9]);
+//   await sources[3].addArticle(articles[3]);
+//   await sources[3].addArticle(articles[10]);
+//   await sources[4].addArticle(articles[4]);
+//   await sources[4].addArticle(articles[11]);
+//   await sources[5].addArticle(articles[5]);
+//   await sources[5].addArticle(articles[12]);
+//   await sources[6].addArticle(articles[6]);
+//   await sources[6].addArticle(articles[13]);
 
-// //   // associate articles with first sample event
-// //   await events[0].addArticle(articles[0]);
-// //   await events[0].addArticle(articles[1]);
-// //   await events[0].addArticle(articles[2]);
-// //   await events[0].addArticle(articles[3]);
-// //   await events[0].addArticle(articles[4]);
-// //   await events[0].addArticle(articles[5]);
-// //   await events[0].addArticle(articles[6]);
+//   // associate articles with first sample event
+//   await events[0].addArticle(articles[0]);
+//   await events[0].addArticle(articles[1]);
+//   await events[0].addArticle(articles[2]);
+//   await events[0].addArticle(articles[3]);
+//   await events[0].addArticle(articles[4]);
+//   await events[0].addArticle(articles[5]);
+//   await events[0].addArticle(articles[6]);
 
-// //   //associate articles with second sample event
-// //   await events[1].addArticle(articles[7]);
-// //   await events[1].addArticle(articles[8]);
-// //   await events[1].addArticle(articles[9]);
-// //   await events[1].addArticle(articles[10]);
-// //   await events[1].addArticle(articles[11]);
-// //   await events[1].addArticle(articles[12]);
-// //   await events[1].addArticle(articles[13]);
+//   //associate articles with second sample event
+//   await events[1].addArticle(articles[7]);
+//   await events[1].addArticle(articles[8]);
+//   await events[1].addArticle(articles[9]);
+//   await events[1].addArticle(articles[10]);
+//   await events[1].addArticle(articles[11]);
+//   await events[1].addArticle(articles[12]);
+//   await events[1].addArticle(articles[13]);
 
-// //   // category event with subcategory
-// //   await categories[0].addSubcategory(subcategories[0]);
-// //   await categories[1].addSubcategory(subcategories[1]);
+//   // category event with subcategory
+//   await categories[0].addSubcategory(subcategories[0]);
+//   await categories[1].addSubcategory(subcategories[1]);
 
-// //   // associate event with category
-// //   await events[0].addSubcategory(subcategories[0]);
-// //   await events[1].addSubcategory(subcategories[1]);
-// // });
+//   // associate event with category
+//   await events[0].addSubcategory(subcategories[0]);
+//   await events[1].addSubcategory(subcategories[1]);
+// });
 
-// /////////////////////////////
+/////////////////////////////
 
-// module.exports = {
-//   Event,
-//   Article,
-//   Concept,
-//   Source,
-//   Category,
-//   Subcategory,
-//   clearTable,
-//   clearDB
-// };
+module.exports = {
+  Event,
+  Article,
+  Concept,
+  Source,
+  Category,
+  Subcategory,
+  clearTable,
+  clearDB
+};
--- server/index.js
+++ server/index.js
@@ -11,44 +11,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/dist'));
 
 // categories
-// app.get('/categories', wrap(async (req, res) => {
-//   const categories = await db.Category.findAll();
-//   res.json(categories);
-// }));
+app.get('/categories', wrap(async (req, res) => {
+  const categories = await db.Category.findAll();
+  res.json(categories);
+}));
 
 // events
-// app.get('/events', wrap(async (req, res) => {
-//   const { categoryId } = req.query;
-//   // TODO: limit events to last X days
-//   const { Subcategories } = await db.Category.findById(categoryId, {
-//     include: [{
-//       model: db.Subcategory,
-//       include: db.Event,
-//     }],
-//   });
+app.get('/events', wrap(async (req, res) => {
+  const { categoryId } = req.query;
+  // TODO: limit events to last X days
+  const { Subcategories } = await db.Category.findById(categoryId, {
+    include: [{
+      model: db.Subcategory,
+      include: db.Event,
+    }],
+  });
 
-//   let events = [];
-//   for (let i = 0; i < Subcategories.length; i++) {
-//     events = events.concat(Subcategories[i].Events);
-//   }
-//   res.json(events);
-// }));
+  let events = [];
+  for (let i = 0; i < Subcategories.length; i++) {
+    events = events.concat(Subcategories[i].Events);
+  }
+  res.json(events);
+}));
 
 // articles
-// app.get('/articles', wrap(async (req, res) => {
-//   const { eventId } = req.query;
+app.get('/articles', wrap(async (req, res) => {
+  const { eventId } = req.query;
 
-//   const sourceUris = ['foxnews.com', 'breitbart.com', 'huffingtonpost.com', 'msnbc.com', 'thehill.com', 'hosted.ap.org', 'nytimes.com'];
-//   const sources = await db.Source.findAll({ where: { uri: sourceUris } });
-//   const sourceIds = sources.map(source => source.dataValues.id);
+  const sourceUris = ['foxnews.com', 'breitbart.com', 'huffingtonpost.com', 'msnbc.com', 'thehill.com', 'hosted.ap.org', 'nytimes.com'];
+  const sources = await db.Source.findAll({ where: { uri: sourceUris } });
+  const sourceIds = sources.map(source => source.dataValues.id);
 
-//   const articles = await db.Article.findAll({
-//     where: {
-//       eventId,
-//       sourceId: sourceIds,
-//     }
-//   });
-//   res.json(articles);
-// }));
+  const articles = await db.Article.findAll({
+    where: {
+      eventId,
+      sourceId: sourceIds,
+    }
+  });
+  res.json(articles);
+}));
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
