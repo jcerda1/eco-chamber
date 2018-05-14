a0f70a985760232dfc9a7fe35ded64981f9fdd10
AlexWellsHS <alex@headspace.com>
2018-05-14 10:28:33 -0700

    add /api to server routes

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const get = (route, params) => axios.get(route, { params }).then(res => res.data);
+const get = (route, params) => axios.get('/api' + route, { params }).then(res => res.data);
 
 module.exports = {
   get,
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const db = require('../models/index');
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 
 const app = express();
@@ -10,19 +11,19 @@ const wrap = fn => (...args) => fn(...args).catch(args[2]);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
 // swagger
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('../swagger.json')));
 
 // categories
-app.get('/categories', wrap(async (req, res) => {
+app.get('/api/categories', wrap(async (req, res) => {
   const categories = await db.Category.findAll();
   res.json(categories);
 }));
 
 // events
-app.get('/events', wrap(async (req, res) => {
+app.get('/api/events', wrap(async (req, res) => {
   const { categoryId } = req.query;
   // TODO: limit events to last X days
   const { Subcategories } = await db.Category.findById(categoryId, {
@@ -47,7 +48,7 @@ app.get('/events', wrap(async (req, res) => {
 }));
 
 // sources
-app.get('/sources', wrap(async (req, res) => {
+app.get('/api/sources', wrap(async (req, res) => {
   const { eventId } = req.query;
   const sourceUris = ['huffingtonpost.com', 'msnbc.com', 'nytimes.com', 'hosted.ap.org', 'thehill.com', 'foxnews.com', 'breitbart.com'];
   const sources = await db.Source.findAll({
@@ -62,5 +63,10 @@ app.get('/sources', wrap(async (req, res) => {
   });
   res.json(sources);
 }));
+
+// serve index.html
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+});
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
