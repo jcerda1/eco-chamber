7571684b3291dfd2a346e2d06632ccf1d50dba2f
jcerda <jccer103@gmail.com>
2018-05-15 23:42:26 -0500

    handles edge cases when articles are not provided

--- client/src/components/ArticleList.jsx
+++ client/src/components/ArticleList.jsx
@@ -6,13 +6,15 @@ class ArticleList extends Component {
   }
 
   render() {
-    const articles = this.props.articles.map(({ id, title, url }) => {
+    const articles = this.props.articles.length ? this.props.articles.map(({ id, title, url }) => {
       return (
         <li key={id}>
           <a style={{"text-decoration": "none", "color": "black"}} href={url} target="_blank">{title}</a>
         </li>
-      );
-    });
+      )
+    }) : 
+    <h2 style={{"fontStyle": "italic"}}> This news source has not reported on this topic </h2>
+      
 
     return (
       <ul className="article-body" >
