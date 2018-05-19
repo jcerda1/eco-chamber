38d2ea8ea2e096d76c6ab02dc9c0eb9fec0f1fa9
AlexWellsHS <alex@headspace.com>
2018-05-19 16:22:14 -0700

    update signup page

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,34 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import Api from '../helpers/Api';
+import Auth from '../helpers/Auth';
 
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
 
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
 
-const Signup = () => (
-  <form>
-    <div>
-      <h1>Register</h1>
-      <p>Please fill in this form to create an account.</p>
-      <hr/>
+  onClick = () => {
+    Api.post('/users', this.state)
+      .then(() => {
+        return Api.get('/auth/login', this.state);
+      })
+      .then(jwt => {
+        Auth.setJWT(jwt);
+        this.props.history.push('/');
+      });
+  }
 
-      <label for="email"><b>Email</b></label>
-      <input type="text" placeholder="Enter Email" name="email" required></input>
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Register</h1>
+          <p>Please fill in this form to create an account.</p>
+          <hr/>
 
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required></input>
+          <label for="email"><b>Email</b></label>
+          <input type="text" placeholder="Enter Email" name="email" value={this.state.email} onChange={this.onChange} required></input>
 
-      <label for="psw-repeat"><b>Repeat Password</b></label>
-      <input type="password" placeholder="Repeat Password" name="psw-repeat" required></input>
-      <hr/>
-      <p>By creating an account you agree to our <Link to="/">Terms & Privacy</Link>.</p>
+          <label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="password" value={this.state.password} onChange={this.onChange} required></input>
 
-      <button type="submit" class="registerbtn">Register</button>
-    </div>
-    <div class="container-signin">
-      <p>Already have an account? <Link to="/signin">Sign in</Link>.</p>
-    </div>
-  </form>
-)
+          <hr/>
+          <p>By creating an account you agree to our <Link to="/">Terms & Privacy</Link>.</p>
 
- 
+          <button type="submit" class="registerbtn" onClick={this.onClick}>Register</button>
+        </div>
+        <div class="container-signin">
+          <p>Already have an account? <Link to="/signin">Sign in</Link>.</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Signup; 
