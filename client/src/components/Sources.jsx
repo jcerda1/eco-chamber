69879680fb95f1aa75ecd4234c579a0032a231de
michellelockett <shelldog42@gmail.com>
2018-05-20 07:42:06 -0700

    users can toggle between sources

--- client/src/components/Sources.jsx
+++ client/src/components/Sources.jsx
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Source from './Source.jsx';
-import ArticleList from './ArticleList.jsx';
+
 
 class Sources extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      sources: this.props.sources,
+      selected: this.props.sources[0],
+      selectedIndex: 0
+    };
+    this.toggleSelected = this.toggleSelected.bind(this);
   }
+
+  toggleSelected(e, leftOrRight) {
+    e.preventDefault();
+    let currentIndex = this.state.selectedIndex;
+    let sources = this.state.sources;
+
+    if (leftOrRight === 'left') {
+      if (currentIndex === 0) {
+        this.setState({ selectedIndex: this.state.sources.length -1, selected: this.state.sources[this.state.sources.length -1]});
+      } else {
+        this.setState({ selectedIndex: currentIndex - 1, selected: this.props.sources[currentIndex - 1] });
+      }    
+    } else {
+      if (currentIndex === this.state.sources.length -1) {
+        this.setState({ selectedIndex: 0, selected: this.props.sources[0] });
+      } else {
+        this.setState({ selectedIndex: currentIndex + 1, selected: this.props.sources[currentIndex + 1] });
+      }
+      
+    }
+  }
+
 
   render() {
     const sourcesAll = this.props.sources.map(({ Articles, ...source }) => {
       return (
         <div key={source.id}>
-          <Source source={source}/>
-          <ArticleList articles={Articles}/>        
+          <Source toggle={this.toggleSelected} articles={Articles} show={this.state.selected.id === source.id ? true: false} source={source}/>       
         </div>
       );
     });
