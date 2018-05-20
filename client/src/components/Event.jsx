ffd041dc7d90afafea70c6b466df7b1005b4e968
michellelockett <shelldog42@gmail.com>
2018-05-20 09:24:46 -0700

    creates word maps for events based on article titles

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -4,12 +4,16 @@ import Api from '../helpers/Api';
 import Sources from './Sources.jsx';
 import ArticleList from './ArticleList.jsx';
 import moment from 'moment';
+import WordMap from './WordMap.jsx';
 
 class Event extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      orderedSources: []
+      orderedSources: [],
+      articles: [],
+      titleWords:{},
+      weightedWords: []
     };
   }
 
@@ -17,8 +21,51 @@ class Event extends Component {
     let { eventId } = this.props.match.params;
     Api.get('/sources', { eventId }).then(sources => {
       this.orderSources(sources);
-    });
+      this.getAllArticles(sources);
+      this.analyzeArticleTitles(this.state.articles);   
+    }); 
   } 
+
+  getAllArticles(allSources) {
+    let allArticles = [];
+    for (const source of allSources) {
+      allArticles = allArticles.concat(source.Articles);
+    }
+    this.setState({articles: allArticles});
+  }
+
+  analyzeArticleTitles(articles) {
+    let ignoredWords = ['of', 'the', 'a', 'an', 'he', 'she', 'and', 'for', 'in', 'on', 'to', 'with', 'over', 'it', 'as', '|'];
+    let words = {};
+    let weighted = [];
+
+    //get word frequency
+    for (const article of articles) {
+    
+      let title = 
+        article.title
+          .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+          .replace(/\s{2,}/g," ")
+          .split(' ').map(x => x.toLowerCase());
+
+      for (const word of title) {
+        if (!words[word] && !ignoredWords.includes(word)) {
+          words[word] = 1;
+        } else if (!ignoredWords.includes(word)) {
+          words[word] ++;
+        }
+      }
+    }
+
+    //extract meaningful words
+    for (const item in words) {
+      if (words[item] > 1) {
+        weighted.push({"text": item, "value": words[item] * 125});
+      }
+
+    }
+    this.setState({ titleWords: words, weightedWords: weighted }, () => console.log(this.state));
+  }
 
   orderSources (allSources) {
     let farLeft = allSources.filter(source => source.bias === -2).sort((a, b) => a.Articles.length < b.Articles.length);
@@ -27,11 +74,10 @@ class Event extends Component {
     let right = allSources.filter(source => source.bias === 1).sort((a, b) => a.Articles.length < b.Articles.length);
     let farRight = allSources.filter(source => source.bias === 2).sort((a, b) => a.Articles.length < b.Articles.length);
     
-    this.setState({ "orderedSources": [ farLeft, left, center, right, farRight] }, () => console.log('sources ordered', this.state));
+    this.setState({ "orderedSources": [ farLeft, left, center, right, farRight] });
   }
 
   render() {
-    const formattedDate = moment(this.props.location.state.date).fromNow();
     const sources = this.state.orderedSources.map(x => {
       return (
         <li key={x[0].bias}>
@@ -42,8 +88,10 @@ class Event extends Component {
 
     return (
       <div>
-        <h1 className="event-title">{this.props.location.state.title}</h1>
-        <h3 className="event-date">{formattedDate}</h3>
+        <div className="word-map">
+          <WordMap data={this.state.weightedWords}/>
+        </div>
+        
         <ul  className="articles-container">
           {sources}
         </ul>
---
+++ client/src/components/WordMap.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from 'react-dom';
+import WordCloud from 'react-d3-cloud';
+
+const fontSizeMapper = word => word.value / 10;
+const rotate = word => (word.value % 90) - 45;
+
+class EventWordCloud extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <WordCloud
+        width={500}
+        height={300}
+        data={this.props.data}
+        fontSizeMapper={fontSizeMapper}
+        rotate={rotate}
+        padding={2}
+      />
+    );
+   } 
+};
+
+export default EventWordCloud
