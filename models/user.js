c64f894c49a7dc54eb39779a9ac13014700bb51b
michellelockett <shelldog42@gmail.com>
2018-06-02 11:45:39 -0700

    change models to reflect user bias and user ratings

--- models/user.js
+++ models/user.js
@@ -2,10 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
     email: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    bias: DataTypes.INTEGER
   }, {});
   User.associate = function(models) {
     User.belongsToMany(models.Event, {through: 'UserEvent'});
+    User.belongsToMany(models.Rating, {through: 'UserRating'});
   };
   return User;
 };
