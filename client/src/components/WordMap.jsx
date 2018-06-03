202a72a19b504ed9aac95f4b6e75577b3f7dce15
michellelockett <shelldog42@gmail.com>
2018-06-03 12:43:18 -0700

    word maps now static in event view

--- client/src/components/WordMap.jsx
+++ client/src/components/WordMap.jsx
@@ -13,7 +13,11 @@ class EventWordCloud extends React.Component {
   shouldComponentUpdate(newProps) {
     const oldData = this.props.data;
     const newData = newProps.data;
-
+    //make sure to render the first time
+    if (oldData.length === 0 && newData.length > 0) {
+      return true;
+    }
+    //only re-render if the word algorithm has changed
     for (const item in oldData) {
       if (oldData[item].text !== newData[item].text || oldData[item].value !== newData[item].value) {
         return true;
