eced89cbad087e574b979536b0f74a5412f04146
michellelockett <shelldog42@gmail.com>
2018-06-04 10:45:02 -0700

    remove checkbox on signin page

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -31,9 +31,6 @@ class Signin extends Component {
           <h2 className="form-signin-heading">Please login</h2>
           <input onChange={this.onChange} type="text" className="form-control" name="email" placeholder="Email Address" required=""/>
           <input onChange={this.onChange} type="password" className="form-control" name="password" placeholder="Password" required=""/>      
-          <label className="checkbox">
-            <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"/> Remember me
-          </label>
           <button className="btn btn-lg btn-primary btn-block" onClick={this.onClick}>Login</button>   
         </div>
       </div>
