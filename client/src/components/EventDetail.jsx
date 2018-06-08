91616e4f31506b634cb8f893a40d4687f35ca7f4
michellelockett <shelldog42@gmail.com>
2018-06-08 10:25:48 -0700

    adds sources to event detail page

--- client/src/components/EventDetail.jsx
+++ client/src/components/EventDetail.jsx
@@ -6,7 +6,7 @@ import EventSentimentRadarChart from './EventSentimentRadarChart.jsx';
 class EventDetail extends Component {
   constructor(props) {
     super(props);
-    this.state = { left: {}, right: {}, center: {} };
+    this.state = { left: {}, right: {}, center: {}, articles: [], sources:[] };
   }
 
   componentDidMount() {
@@ -14,13 +14,45 @@ class EventDetail extends Component {
     Api.get('/eventSentiment', { eventId }).then(sentiments => {
       this.setState({ left: sentiments.left , right: sentiments.right, center: sentiments.center });  
     }); 
+    Api.get('/events/articles', { eventId }).then(event => {
+      let sources = this.extractSources(event.Articles);
+      this.setState({articles: event.Articles, sources: sources}, ()=> console.log(this.state));
+    });
+  }
+
+  extractSources(articles) {
+    let sourceUris = [];
+    let sources = [];
+
+    for (const article of articles) {
+      if (!sourceUris.includes(article.Source.uri)) {
+        let source = {image: article.Source.image, title: article.Source.title};
+        sourceUris.push(article.Source.uri);
+        sources.push(source);
+      }
+    }
+    return sources;
   }
 
   render() {
+    const sources = this.state.sources.map(source => {
+      return (
+        <div key={source.id} className="event-source">
+          <img src={source.image}></img>
+          <h3>{source.title}</h3>
+        </div>
+      )
+    })
+
     return (
       <div className="event-detail">     
         <h1>{this.props.title}</h1>
-        <h2>REPORTING SOURCES HERE</h2>
+        <h2>REPORTING SOURCES</h2>
+
+        <div className="event-sources">
+          {sources}
+        </div>
+
         <div className="event-charts">
           <div className="event-radar">
             <h2>EVENT EMOTION ANALYSIS</h2>
@@ -29,7 +61,8 @@ class EventDetail extends Component {
               right={this.state.right}
               center={this.state.center}
               eventId={this.props.eventId} />
-          </div>        
+          </div> 
+
           <h2>LABEL DATA HERE</h2> 
         </div>  
       </div>     
