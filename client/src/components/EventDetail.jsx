333f59f80414436e5d1faaa3cfbaa72c2e6ec0d8
michellelockett <shelldog42@gmail.com>
2018-06-08 09:32:37 -0700

    moves sentiment call up to eventdetail page

--- client/src/components/EventDetail.jsx
+++ client/src/components/EventDetail.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import Api from '../helpers/Api';
 import moment from 'moment';
 import EventSentimentRadarChart from './EventSentimentRadarChart.jsx';
 
 class EventDetail extends Component {
   constructor(props) {
     super(props);
+    this.state = { left: {}, right: {}, center: {} };
+  }
+
+  componentDidMount() {
+    let { eventId } = this.props;
+    Api.get('/eventSentiment', { eventId }).then(sentiments => {
+      this.setState({ left: sentiments.left , right: sentiments.right, center: sentiments.center });  
+    }); 
   }
 
   render() {
     return (
       <div className="event-detail">     
         <h1> Information about this event </h1>  
-        <EventSentimentRadarChart eventId={this.props.eventId} />   
+        <EventSentimentRadarChart 
+          left={this.state.left} 
+          right={this.state.right}
+          center={this.state.center}
+          eventId={this.props.eventId} />   
       </div>     
     );
   }
