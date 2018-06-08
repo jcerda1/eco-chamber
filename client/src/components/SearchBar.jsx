103c8cf8bd9d5a145594127d400b0b37b89accaf
AlexWellsHS <alex@headspace.com>
2018-06-08 13:57:40 -0700

    refactor and style search bar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+const FaSearch = require('react-icons/lib/fa/search');
 
 class SearchBar extends Component {
   constructor(props) {
@@ -14,14 +15,28 @@ class SearchBar extends Component {
     this.setState({ value });
   };
 
+  onKeyUp = (e) => {
+    if (e.key === 'Enter') {
+      this.props.history.push(`/search-results/${this.state.value}`);
+    }
+  }
+
   render() {
     return (
       <div className="search-bar-container">
-        <input placeholder="Search events..." onChange={this.onChange} className="search-input"/>
-        <Link to={`/search-results/${this.state.value}`}>&#x1F50D;</Link>
+        <input
+          onKeyUp={this.onKeyUp}
+          onChange={this.onChange}
+          size="40"
+          className="search-input"
+          placeholder="Search events..."
+        />
+        <Link to={`/search-results/${this.state.value}`}>
+          <FaSearch style={{ color: 'white', fontSize: '20', marginTop: '0' }}/>
+        </Link>
       </div>
     );
   }
 };
 
-export default SearchBar;
+export default withRouter(SearchBar);
